16933ea17e593cacc2d514f86bbc5c6eaacbf36a
Letícia Cúgnier Gonçalves <contato.leticiacugnier@gmail.com>
2020-03-02 22:25:21 -0300

    Implantação da API pokeApi.co

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DadosService } from '../servicos/dados.service';
 import { Router } from '@angular/router';
 import { IPokemon } from '../interfaces/iPokemon';
+import { PokemonApiService } from '../servicos/pokemon-api.service';
 
 @Component({
   selector: 'app-home',
@@ -33,11 +34,28 @@ export class HomePage {
   ];
   public listaFiltrada=[];
 
+  public listaPokemonApi;
+  public totalPokemons;
+  public offset = 0;
+  public limit = 10;
+
   constructor(
     public dadosService: DadosService,
-    public router: Router
+    public router: Router,
+    public pokeApi: PokemonApiService
     ) {
     this.resetarLista();
+    this.buscaPokemons(this.offset, this.limit);
+  }
+
+  public buscaPokemons(offset,limit){
+    this.pokeApi.buscaPokemons(offset,limit).subscribe(dados=>{console.log(dados);
+      //Pega somente o total de pokemons
+      this.totalPokemons = dados['count'];
+      // Pega somente a lista de pokemon
+      this.listaPokemonApi = dados['result'];
+
+    })
   }
 
   abrirDadosPokemon(pokemon: IPokemon){
